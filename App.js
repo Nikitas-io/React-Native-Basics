b6a3cdc4e03f5ff1b5efb2e902ec4577adf1a537
Nikitas I/O <chrononautic@gmail.com>
2023-01-26 19:44:52 +0200

    Started working on some basic styles for a goal list

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}>
-        Hello world!
-      </Text>
-      <Button title="Tap me!" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Write your goal"/>
+        <Button title="Add goal"/>
+      </View>
+      <View style={styles.goalContainer}>
+        <Text style={styles.goalList}>The goal list: </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    flex: 1, // To make sure that the app container takes up all the available height on the screen.
+    paddingTop: 50,
+    paddingHorizontal: 15,
+    justifyContent: "space-evenly"
   },
+  inputContainer: {
+    height: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "gray",
+    width: "70%",
+    padding: 8
+  },
+  goalContainer: {
+    flex: 1
+  },
+  goalList: {
+    borderTopColor: "gray",
+    borderTopWidth: 1,
+    marginTop: 23,
+    paddingTop: 10
+  }
 });
